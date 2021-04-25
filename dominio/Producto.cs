464306c7217e5e25e24f472bf201198e1015a551
Ferrante, Bruno <bruno.ferrante@alumnos.frgp.utn.edu.ar>
2021-04-24 22:29:54 -0300

    test

---
+++ dominio/Producto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Producto
+    {
+        public int ID { get; set; }
+
+        public int codigo { get; set; }
+
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public float stock { get; set; }
+
+        public Marca marca { get; set; }
+
+        public Categoria categoria { get; set; }
+
+        public string urlImagen { get; set; }
+
+        public float precio { get; set; }
+
+        public bool estado { get; set; }
+
+
+    }
+}
