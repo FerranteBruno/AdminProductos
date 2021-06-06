1986bdae45cacc8c6b8aab68cec56dc5aea7d294
Aldebaran14D <69329183+Aldebaran14D@users.noreply.github.com>
2021-06-06 19:54:56 -0300

    Dominio: Carrito

---
+++ dominio/Carrito.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Carrito
+    {
+        public List<itemCarrito> listado { get; set; }
+        public double total { get; set; }
+    }
+    public class itemCarrito
+    {
+        public int id { get; set; }
+        public Producto item { get; set; }
+        public int cantidad { get; set; }
+        public double subtotal { get; set; }
+    }
+}
